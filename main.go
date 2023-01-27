3652504778d5c1c03966c86c5242241ab4c65371
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2023-01-27 17:22:06 +0700

    main.go

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"pgp-wasm/pgp"
+)
+
 func main() {
+	var (
+		name       = "Lugon Dev"
+		email      = "lugon@alphatrue.com"
+		passphrase = []byte("LongSecret")
+	)
+	pgpRsa, err := pgp.GenerateKeyArmor(&pgp.KeyParam{
+		Name:       name,
+		Email:      email,
+		Passphrase: passphrase,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.Marshal(pgpRsa)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jsonData))
 }
