cd7bcb569f17c8b87d5e896cee9bb3926df129b4
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2023-01-27 14:58:29 +0700

    clean main.go

--- main.go
+++ main.go
@@ -1,30 +1,4 @@
 package main
 
-import (
-	"encoding/hex"
-	"fmt"
-	"github.com/ProtonMail/gopenpgp/v2/helper"
-)
-
 func main() {
-	var (
-		name       = "Max Mustermann"
-		email      = "max.mustermann@example.com"
-		passphrase = []byte("LongSecret")
-		rsaBits    = 2048
-	)
-	// RSA, string
-	rsaKey, err := helper.GenerateKey(name, email, passphrase, "rsa", rsaBits)
-	if err != nil {
-		panic(err)
-	}
-	hexKey := hex.EncodeToString([]byte(rsaKey))
-	fmt.Println(hexKey)
-
-	bytesKey, err := hex.DecodeString(hexKey)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(bytesKey))
 }
